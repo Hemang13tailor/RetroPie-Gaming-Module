2ef2bf1792c39f53db443f0265bcb73aa3e8b369
Hemang Tailor <hemangtailor13@gmail.com>
2024-07-08 20:37:07 +0530

    The Arduino IDE code for the Gaming Module

---
+++ Tinkering_Project.ino
@@ -0,0 +1,162 @@
+
+#include <Arduino.h>
+#include <BleGamepad.h>
+
+//ABXY BUTTONS
+#define X_BUTTON 23        // A
+#define CIRCLE_BUTTON 22    // B
+#define TRIANGLE_BUTTON 1  // Y
+#define SQUARE_BUTTON 3    // X
+
+//TRIGGERS
+#define R1_BUTTON 0
+#define R2_BUTTON 0
+#define L1_BUTTON 0
+#define L2_BUTTON 0
+
+//MENU BUTTONS
+#define START_BUTTON 0
+#define SELECT_BUTTON 19
+#define PS_BUTTON 0
+
+//JOYSTICKS BUTTONS
+#define R3_BUTTON 0
+#define L3_BUTTON 0
+
+//JOYSTICKS
+#define LEFT_VRX_JOYSTICK 4
+#define LEFT_VRY_JOYSTICK 15
+#define RIGHT_VRX_JOYSTICK 0
+#define RIGHT_VRY_JOYSTICK 0
+
+#define NUM_BUTTONS 13
+
+//Defining main array which will contain the order of buttons.
+int buttonsPins[NUM_BUTTONS] = {X_BUTTON, CIRCLE_BUTTON, TRIANGLE_BUTTON, SQUARE_BUTTON,
+                          R1_BUTTON, R2_BUTTON, L1_BUTTON, L2_BUTTON,
+                          START_BUTTON, SELECT_BUTTON, PS_BUTTON,
+                          R3_BUTTON, L3_BUTTON};
+
+//Defining array for various Gamepads
+int androidGamepadButtons[NUM_BUTTONS] = {1, 2, 3, 4, 8, 10, 7, 9, 12, 11, 13, 15, 14};
+int PS1GamepadButtons[NUM_BUTTONS] = {2, 3, 4, 1, 6, 8, 5, 7, 10, 9, 13, 12, 11};
+int PCGamepadButtons[NUM_BUTTONS] = {1, 2, 4, 3, 6, 8, 5, 7, 10, 9, 0, 12, 11};
+
+//joysticks values
+uint16_t leftVrxJoystickLecture = 0;
+uint16_t leftVryJoystickLecture = 0;
+uint16_t rightVrxJoystickLecture = 0;
+uint16_t rightVryJoystickLecture = 0;
+
+uint16_t leftVrxJoystickValue = 0;
+uint16_t leftVryJoystickValue = 0;
+uint16_t rightVrxJoystickValue = 0;
+uint16_t rightVryJoystickValue = 0;
+
+//gamepad mode
+typedef enum{ANDROID, PS1, PC} GamepadModes;
+GamepadModes gamepadMode = ANDROID;
+
+
+//code for bluetooth connectivity via function provided in blegamepad library
+BleGamepad bleGamepad("Maker101 Gamepad", "Maker101 Home");
+BleGamepadConfiguration bleGamepadConfig;  
+
+
+void ICACHE_RAM_ATTR ISRoutine ();
+
+
+//setup
+void setup() {
+  // put your setup code here, to run once:
+  delay(1000);
+  Serial.begin(115200);
+
+
+  for(int i=0; i<NUM_BUTTONS; i++){
+    pinMode(buttonsPins[i], INPUT_PULLUP);
+  }
+
+  bleGamepadConfig.setAutoReport(false);
+  bleGamepadConfig.setControllerType(CONTROLLER_TYPE_GAMEPAD); // CONTROLLER_TYPE_JOYSTICK, CONTROLLER_TYPE_GAMEPAD (DEFAULT), CONTROLLER_TYPE_MULTI_AXIS
+  bleGamepadConfig.setVid(0xe502);
+  bleGamepadConfig.setPid(0xabcd);
+  bleGamepadConfig.setHatSwitchCount(4);
+  bleGamepad.begin(&bleGamepadConfig);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(bleGamepad.isConnected()){
+
+     Serial.println("Code connected...");
+    //Joysticks lecture
+    leftVrxJoystickLecture = analogRead(LEFT_VRX_JOYSTICK);
+    leftVryJoystickLecture = analogRead(LEFT_VRY_JOYSTICK);
+    rightVrxJoystickLecture = analogRead(RIGHT_VRX_JOYSTICK);
+    rightVryJoystickLecture = analogRead(RIGHT_VRY_JOYSTICK);
+
+    //Compute joysticks value
+    leftVrxJoystickValue = map(leftVrxJoystickLecture, 4095, 0, 0, 32737);
+    leftVryJoystickValue = map(leftVryJoystickLecture, 0, 4095, 0, 32737);
+    rightVrxJoystickValue = map(rightVrxJoystickLecture, 4095, 0, 0, 32737);
+    rightVryJoystickValue = map(rightVryJoystickLecture, 0, 4095, 0, 32737);
+
+    
+    switch(gamepadMode){
+      case ANDROID:
+        for(int i=0; i<NUM_BUTTONS; i++){
+          if(!digitalRead(buttonsPins[i])){
+              bleGamepad.press(androidGamepadButtons[i]);   
+          }
+          else{
+              bleGamepad.release(androidGamepadButtons[i]);     
+          }
+          joysticksHandlerForMobile(leftVrxJoystickValue, leftVryJoystickValue, rightVrxJoystickValue, rightVryJoystickValue);
+        } 
+        break;
+
+      case PS1:
+        for(int i=0; i<NUM_BUTTONS; i++){
+          if(!digitalRead(buttonsPins[i])){
+            bleGamepad.press(PS1GamepadButtons[i]);     
+          }
+          else{
+            bleGamepad.release(PS1GamepadButtons[i]);      
+          }
+          joysticksHandlerForMobile(leftVrxJoystickValue, leftVryJoystickValue, rightVrxJoystickValue, rightVryJoystickValue);
+        } 
+        break;
+
+        case PC:
+          for(int i=0; i<NUM_BUTTONS; i++){
+            if(!digitalRead(buttonsPins[i])){
+              bleGamepad.press(PCGamepadButtons[i]);
+            }
+            else{
+              bleGamepad.release(PCGamepadButtons[i]);
+            }
+            joysticksHandlerForPC(leftVrxJoystickValue, leftVryJoystickValue, rightVrxJoystickValue, rightVryJoystickValue);
+          }
+          break;
+    }
+
+    bleGamepad.sendReport();
+  }
+}
+
+void joysticksHandlerForMobile(uint16_t leftVrx, uint16_t leftVry, uint16_t rightVrx, uint16_t rightVry){
+  bleGamepad.setLeftThumb(leftVrx, leftVryJoystickValue);
+  bleGamepad.setRightThumb(rightVrxJoystickValue, rightVryJoystickValue);  
+}
+
+void joysticksHandlerForPC(uint16_t leftVrx, uint16_t leftVry, uint16_t rightVrx, uint16_t rightVry){
+  bleGamepad.setX(leftVrxJoystickValue);
+  bleGamepad.setY(leftVryJoystickValue);
+  bleGamepad.setZ(rightVrxJoystickValue);
+  bleGamepad.setRX(rightVryJoystickValue);
+}
+
+
+
+
